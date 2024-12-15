173aa39685ac97f51823ae827f6d5065739ec785
toothlessdev <toothless042@gmail.com>
2024-12-15 22:51:49 +0900

    chore: site configuration 구성

---
+++ src/config/site.ts
@@ -0,0 +1,26 @@
+export const siteConfiguration = {
+    menu: [
+        {
+            label: "Home",
+            path: "/",
+        },
+        {
+            label: "About",
+            path: "/about",
+        },
+        {
+            label: "Posts",
+            path: "/posts",
+        },
+        {
+            label: "Projects",
+            path: "/projects",
+        },
+    ],
+
+    externalLinks: {
+        github: "https://www.github.com/toothlessdev",
+        instagram: "https://www.instagram.com/toothlesskid_",
+        linkedin: "",
+    },
+};
