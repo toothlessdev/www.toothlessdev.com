2bb60b7eb867a319522efd1df6ac5264695976e7
toothlessdev <toothless042@gmail.com>
2024-12-16 21:35:28 +0900

    fix: useSearchParams Suspense 오류 해결

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import { CategoryMenu } from "@/entities/category/ui/CategoryMenu";
 import { PageInfo } from "@/entities/filter/ui/PageInfo";
 import { Section } from "@/entities/section/ui/Section";
@@ -8,26 +9,28 @@ export default function PostsPage() {
     const { posts } = usePostPreviews();
 
     return (
-        <div className="flex">
-            <CategoryMenu />
-            <Section
-                title={<h1 className="font-semibold">JavaScript Posts</h1>}
-                className="flex-1 p-2"
-            >
-                {posts.map((post, index) => {
-                    return (
-                        <PostListItem
-                            key={index}
-                            title={post.title}
-                            slug={post.slug}
-                            content={post.content}
-                            category={post.category}
-                            createdAt={post.createdAt}
-                        />
-                    );
-                })}
-                <PageInfo totalPage={27} pageGroupSize={5} />
-            </Section>
-        </div>
+        <Suspense>
+            <div className="flex">
+                <CategoryMenu />
+                <Section
+                    title={<h1 className="font-semibold">JavaScript Posts</h1>}
+                    className="flex-1 p-2"
+                >
+                    {posts.map((post, index) => {
+                        return (
+                            <PostListItem
+                                key={index}
+                                title={post.title}
+                                slug={post.slug}
+                                content={post.content}
+                                category={post.category}
+                                createdAt={post.createdAt}
+                            />
+                        );
+                    })}
+                    <PageInfo totalPage={27} pageGroupSize={5} />
+                </Section>
+            </div>
+        </Suspense>
     );
 }
