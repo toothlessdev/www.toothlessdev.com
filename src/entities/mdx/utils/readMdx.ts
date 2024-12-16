bbb3d0c1b40b13efb74067918bb8400c658733bb
toothlessdev <toothless042@gmail.com>
2024-12-16 19:55:51 +0900

    feature: frontMatter, content 를 반환하는 readMdx 유틸리티 구현

--- src/entities/mdx/utils/readMdx.ts
+++ src/entities/mdx/utils/readMdx.ts
@@ -12,3 +12,9 @@ export function readMdxContent(mdxPath: string) {
     const content = matter(mdxContents).content;
     return content;
 }
+
+export function readMdx(mdxPath: string): { frontMatter: Record<string, any>; content: string } {
+    const mdxContents = fs.readFileSync(mdxPath, "utf8");
+    const { data, content } = matter(mdxContents);
+    return { frontMatter: data, content };
+}
