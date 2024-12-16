2b77944745b08763f9e659e222e36ec288d4a3ba
toothlessdev <toothless042@gmail.com>
2024-12-16 19:59:38 +0900

    feature: ExternalLink 컴포넌트 구현

--- src/shared/components/ExternalLink.tsx
+++ src/shared/components/ExternalLink.tsx
@@ -15,3 +15,16 @@ export const ExternalLink = ({ href, children }: ExternalLinkProps) => {
         </a>
     );
 };
+
+export const ExternalLinkButton = ({ href, children }: ExternalLinkProps) => {
+    return (
+        <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href={href}
+            className="inline-flex h-8 items-center justify-center gap-2 whitespace-nowrap rounded-md bg-primary px-4 py-2 text-sm font-medium text-primary-foreground ring-offset-background transition-colors hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0"
+        >
+            {children}
+        </a>
+    );
+};
