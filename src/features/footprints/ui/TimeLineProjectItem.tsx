44e3216096907a385ebdf7fe0876d4de58175716
toothlessdev <toothless042@gmail.com>
2024-12-16 19:58:08 +0900

    feature: FootPrintsPage, TimeLine 컴포넌트 구현

---
+++ src/features/footprints/ui/TimeLineProjectItem.tsx
@@ -0,0 +1,60 @@
+import { Code, Ellipsis, Github, Link } from "lucide-react";
+import { TimeLineItem } from "@/features/footprints/ui/TimeLineItem";
+import { ExternalLinkButton } from "@/shared/components/ExternalLink";
+
+export interface TimeLineProjectItemProps {
+    title: string;
+
+    projectName: string;
+    description: string;
+    githubLink?: string;
+    deploymentLink?: string;
+    projectDetailLink?: string;
+}
+
+export const TimeLineProjectItem = ({
+    title,
+    projectName,
+    description,
+    githubLink,
+    deploymentLink,
+    projectDetailLink,
+}: TimeLineProjectItemProps) => {
+    return (
+        <TimeLineItem
+            title={title}
+            icon={<Code size={14} />}
+            content={
+                <div className="rounded-[10px] border-[1px] border-[#3c444d] p-6">
+                    <div className="flex items-center">
+                        <h2 className="px-1 text-xl font-bold text-white">{projectName}</h2>
+                    </div>
+                    <div className="mt-2">
+                        <p className="line-clamp-2">{description}</p>
+
+                        <div className="ml-auto mr-0 flex justify-end gap-2">
+                            {githubLink && (
+                                <ExternalLinkButton href={githubLink}>
+                                    <Github />
+                                    Github
+                                </ExternalLinkButton>
+                            )}
+                            {deploymentLink && (
+                                <ExternalLinkButton href={deploymentLink}>
+                                    <Link />
+                                    Link
+                                </ExternalLinkButton>
+                            )}
+                            {projectDetailLink && (
+                                <ExternalLinkButton href={projectDetailLink}>
+                                    <Ellipsis />
+                                    Detail
+                                </ExternalLinkButton>
+                            )}
+                        </div>
+                    </div>
+                </div>
+            }
+        />
+    );
+};
